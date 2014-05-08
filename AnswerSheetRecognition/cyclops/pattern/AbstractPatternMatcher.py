e4fc51a914593de98f328e305d4b9d80cb858c51
Felipe Dornelas <felipead@gmail.com>
2014-05-08 04:21:30 -0300

    Code improvement

--- AnswerSheetRecognition/cyclops/pattern/AbstractPatternMatcher.py
+++ AnswerSheetRecognition/cyclops/pattern/AbstractPatternMatcher.py
@@ -1,12 +1,10 @@
 from abc import *
-
-from cv2 import floodFill, cvtColor, matchTemplate, normalize, threshold, minMaxLoc, NORM_MINMAX
-from cv import CV_BGR2GRAY, CV_TM_CCOEFF_NORMED, CV_THRESH_TOZERO
-
+import cv2
 import numpy as np
 
 from PatternFactory import *
 from PatternMatch import *
+from ..util.ImageProcessingUtil import *
 
 class AbstractPatternMatcher:
 
@@ -16,7 +14,7 @@ class AbstractPatternMatcher:
         self._patterns = []
         for patternSize in patternSizes:
             pattern = PatternFactory.getPattern(patternName, patternSize)
-            pattern = cvtColor(pattern, CV_BGR2GRAY)
+            pattern = ImageProcessingUtil.convertToGrayscale(pattern)
             self._patterns.append(pattern)
 
     def match(self, picture, numberOfTimesToMatch):
@@ -26,21 +24,20 @@ class AbstractPatternMatcher:
         return matches
 
     def _match(self, picture, pattern, numberOfTimesToMatch):
-        picture = cvtColor(picture, CV_BGR2GRAY)
-        result = matchTemplate(picture, pattern, CV_TM_CCOEFF_NORMED)
-        result = normalize(result, result, 0, 1, NORM_MINMAX, -1)
-        _, result = threshold(result, 0.5, 1, CV_THRESH_TOZERO)
+        picture = ImageProcessingUtil.convertToGrayscale(picture)
+        result = cv2.matchTemplate(picture, pattern, cv2.TM_CCOEFF_NORMED)
+        result = cv2.normalize(result, result, 0, 1, cv2.NORM_MINMAX, -1)
+        _, result = cv2.threshold(result, 0.5, 1, cv2.THRESH_TOZERO)
 
         patternSize = pattern.shape[:2]
         
         matches = []
         for i in xrange(numberOfTimesToMatch):
-            _, _, _, maxLocation = minMaxLoc(result)
+            _, _, _, maxLocation = cv2.minMaxLoc(result)
             matches.append(PatternMatch(maxLocation, patternSize))
 
             h, w = result.shape[:2]
             mask = np.zeros((h+2, w+2), np.uint8)
-            floodFill(result, mask, maxLocation, (0, 0, 0), loDiff=(.1,.1,.1), upDiff=(1.,1.,1.))
-            # C++ equivalent: cv::floodFill(result, maxLocation, Scalar(0), 0, Scalar(.1), Scalar(1.));
+            cv2.floodFill(result, mask, maxLocation, (0, 0, 0), loDiff=(.1,.1,.1), upDiff=(1.,1.,1.))
 
         return matches
