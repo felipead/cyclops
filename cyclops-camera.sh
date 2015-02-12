1514a566950df8f12e4f385edd76440dfda74c3b
Felipe Dornelas <felipead@gmail.com>
2015-02-12 03:47:14 -0200

    Fixing the .sh scripts that got corrupted during the merge.

--- cyclops-camera.sh
+++ cyclops-camera.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 /usr/bin/env python2.7 -m cyclops.app.CameraApp $@
-=======
-python -m cyclops.app.CameraApp $@
->>>>>>> parent of a566228... Cleaned-up master, preparing for the new C# .NET / Xamarin based implementation.
--- cyclops.sh
+++ cyclops.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 /usr/bin/env python2.7 -m cyclops.app.CommandLineApp $@
-=======
-python -m cyclops.app.CommandLineApp $@
->>>>>>> parent of a566228... Cleaned-up master, preparing for the new C# .NET / Xamarin based implementation.
